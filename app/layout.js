bc276f005957d2471e2bde1d5d960cf4fb06172d
SprayOnShoesDude <tatertotfox2025@gmail.com>
2026-03-11 00:13:56 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;1,300;1,400&family=Nunito:wght@300;400;500;600&display=swap"
           rel="stylesheet"
